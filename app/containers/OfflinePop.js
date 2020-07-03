9951dc64a8d3434c70b381f69f2ca0ea08813dad
Gautam Gireesh <gautam.gireesh@mail.utoronto.ca>
2020-07-02 23:04:40 -0400

    Added wifi icon

--- app/containers/OfflinePop.js
+++ app/containers/OfflinePop.js
@@ -2,14 +2,20 @@ import React, { PureComponent } from 'react';
 import { View, Text, Dimensions, StyleSheet, NetInfo, Image, ImageBackground} from 'react-native';
 import styles from "../assets/styles/index";
 // import NetInfo from "@react-native-community/netinfo";
+import Wifi_Icon from '../assets/icons/wifi.svg';
 
 function MiniOfflineSign() {
     return (
       <ImageBackground source={require('../assets/images/15.png')} style={styles.internetBG}>
+        <View style={{flexDirection: 'column', alignItems: 'center', padding: 20}}>
           <Image style={styles.internetLogo} source={require('../assets/images/Findr_logo2x.png')} />
-          <Text style={styles.internetText}>It seems like your internet is down</Text>
+          <View style={{marginTop: Dimensions.get('window').height * 0.1, padding: 20, flexDirection: 'column', alignItems: 'center'}}>
+            <Wifi_Icon width={40} height={40}/>
+            <Text style={styles.internetText}>Oops! Lost connection</Text>
+          </View>
+        </View>
       </ImageBackground>
-    );
+);
   }
   
   class OfflineNotice extends PureComponent {
