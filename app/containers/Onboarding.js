9c3ab6764783a99be4705513441d8772f09e1c63
Ibrahim Fazili <fazili.ibrahim@gmail.com>
2020-07-10 11:51:36 -0400

    removed extras from view on scrollview

--- app/containers/Onboarding.js
+++ app/containers/Onboarding.js
@@ -76,34 +76,7 @@ class Onboarding extends React.Component {
               this.setState({ idxActive: newIndex });
             }}
             ref={"swiper"}
-            // dot={
-            //   <View
-            //     style={{
-            //       backgroundColor: "rgba(0,0,0,.3)",
-            //       width: FULL_HEIGHT *0.012,
-            //       height: FULL_HEIGHT * 0.012,
-            //       borderRadius: FULL_HEIGHT * 0.012,
-            //       marginLeft: 7,
-            //       marginRight: 7,
-            //       marginBottom: FULL_HEIGHT * 0.15,
-            //     }}
-            //   />
-            // }
-            // activeDot={
-            //   <View
-            //     style={{
-            //       backgroundColor: "#FFF",
-            //       width: FULL_HEIGHT * 0.013,
-            //       height: FULL_HEIGHT * 0.013,
-            //       borderRadius: FULL_HEIGHT * 0.012,
-            //       marginRight: 7,
-            //       marginLeft: 7,
-            //       marginBottom: FULL_HEIGHT * 0.15,
-            //     }}
-            //   />
-            // }
             
-            //Ignore the css styling for this, need not be relative. Only used to hide the dots in swiper.
             paginationStyle={{
               bottom: -23,
               left: null,
@@ -139,7 +112,7 @@ class Onboarding extends React.Component {
                   }}
                 >
                   {this.state.cards.map((user) => (
-                    <View>
+                    <View style={{paddingHorizontal: FULL_WIDTH * 0.006}}>
                       <CardItem
                         variant
                         key={user.name}
