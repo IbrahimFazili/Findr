218c472c587f1b13b9885459a2c9ddc1e824fccd
Aditi Dagar <aditi.dagar@mail.utoronto.ca>
2020-07-09 20:15:56 -0400

    implemented chat encryption

--- backend/utils/Chat.js
+++ backend/utils/Chat.js
@@ -1,13 +1,6 @@
 const DB = require('./DatabaseManager');
 const { EventQueue, Event, MESSAGE_EVENT } = require('./Events');
 
-const MAX_N = 1024;
-const MIN_N = 512;
-/**
- * 1) On new chat: init generator & cyclic group (Z mod(n))
- * 2) Users get their private keys (0 <= key < n) & store them locally
- * 3) 
- */
 class Message {
 	constructor(user, msg, timestamp) {
 		this.user = user;
@@ -20,10 +13,6 @@ class Chat {
 	constructor(user1, user2) {
 		this.user1 = user1;
 		this.user2 = user2;
-		// generate cyclic group
-		this.cyclic_group = Math.floor(Math.random() * (MAX_N - MIN_N + 1)) + MIN_N;
-		// generate the generator
-		this.generator = Math.floor(Math.random() * (this.cyclic_group));
 
 		this.user1_public_key = null;
 		this.user2_public_key = null;
@@ -64,52 +53,48 @@ function bindSocketListeners(io) {
 					var msgHandled = false;
 					const receiverIsReachable = io.sockets.adapter.rooms[msg.to] && io.sockets.adapter.rooms[msg.to].length > 0;
 	
-					for (let i = 0; i < user.chats.length && !msgHandled; i++) {
-						try {
-							chat = (await DB.fetchChat(user.chats[i]))[0].chat;
-							
-							if (chat.user1 === msg.to || chat.user2 === msg.to) {
-								chat = Chat.parseJSON(chat);
-	
-								chat.newMessage(msg.from, msg.content, msg.time, msg.public_key);
-								msgHandled = true;
-	
-								try {
-									await DB.updateChat(chat, {
-										_id: user.chats[i],
-									});
-	
-									if (receiverIsReachable) {
-										socket.to(msg.to).emit("new msg", msg);
-									} else {
-										// store message event in eventQueue to notify user later
-										DB.fetchUsers({ email: msg.to }).then((receiver) => {
-											receiver = receiver[0];
-											receiver.eventQueue = new EventQueue(receiver.eventQueue.events);
-											receiver.eventQueue.enqueue(new Event(MESSAGE_EVENT, {
-												from: msg.from,
-												content: msg.content,
-												time: msg.time
-											}));
-	
-											DB.updateUser({ eventQueue: receiver.eventQueue }, { email: msg.to });
-	
-										}).catch((reason) => {
-											console.log(reason);
-										})
-									}
-								} catch (err_nested) {
-									console.log(err_nested);
-									socket.emit("send failed");
-								}
-							}
-						} catch (err) {
-							console.log(err);
-							socket.emit("server error");
+					try {
+						chat = await findChat(user, msg.to);
+
+						if (chat !== null) {
+							chat.newMessage(msg.from, msg.content, msg.time, msg.public_key);
+							if (msg.public_key) delete msg.public_key;
 							msgHandled = true;
+
+							try {
+								await DB.updateChat(chat, {
+									_id: user.chats[i],
+								});
+
+								if (receiverIsReachable) {
+									socket.to(msg.to).emit("new msg", msg);
+								} else {
+									// store message event in eventQueue to notify user later
+									DB.fetchUsers({ email: msg.to }).then((receiver) => {
+										receiver = receiver[0];
+										receiver.eventQueue = new EventQueue(receiver.eventQueue.events);
+										receiver.eventQueue.enqueue(new Event(MESSAGE_EVENT, {
+											from: msg.from,
+											content: msg.content,
+											time: msg.time
+										}));
+
+										DB.updateUser({ eventQueue: receiver.eventQueue }, { email: msg.to });
+
+									}).catch((reason) => {
+										console.log(reason);
+									})
+								}
+							} catch (err_nested) {
+								console.log(err_nested);
+								socket.emit("send failed");
+							}
 						}
+					} catch (findChatError) {
+						console.log(findChatError);
+						msgHandled = true;
+						socket.emit("server error");
 					}
-	
 					// no existing chat b/w users, so create a new one
 					if (!msgHandled) {
 						initNewChat(socket, user, msg, receiverIsReachable);
@@ -119,6 +104,35 @@ function bindSocketListeners(io) {
 					socket.emit("server error");
 				});
 		});
+
+		socket.on("request public key", (src_email, target_email) => {
+				DB.fetchUsers({ email: src_email })
+				.then(async (users) => {
+					const user = users[0];
+					chat = await findChat(user, target_email);
+					if (chat !== null){
+						if (chat.user1 === user){
+							socket.emit("public key", {
+								user: chat.user2,
+								public_key: chat.user2_public_key
+							});
+						}
+						else {
+							socket.emit("public key", {
+								user: chat.user1,
+								public_key: chat.user1_public_key
+							});
+						}	
+					}
+					else{
+						socket.emit("chat doesn't exist");
+					}
+				})
+				.catch((err) => {
+					socket.emit("server error");
+				});
+		});
+
 	});
 }
 
@@ -178,6 +192,25 @@ function initNewChat(socket, user, msg, receiverIsReachable) {
 			socket.emit("send failed");
 			console.log(err);
 		});
+}
+
+async function findChat(user, target) {
+	for (let i = 0; i < user.chats.length; i++) {
+		try {
+			const chat = (await DB.fetchChat(user.chats[i]))[0].chat;
+
+			if (chat.user1 === target || chat.user2 === target) {
+				chat = Chat.parseJSON(chat);
+				return chat;
+			}
+		} catch (error) {
+			console.log(error);
+			return null;
+		}
+		
+	}
+
+	return null;
 }
 
 module.exports.Chat = Chat;
