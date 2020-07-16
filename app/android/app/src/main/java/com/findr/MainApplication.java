e58cbc1528f8499032196c7e07d42bd52b6ba2e2
Parshva Shah <parshvashah414@gmail.com>
2020-07-16 14:17:37 -0400

    Fixed ios loading bug

--- app/android/app/src/main/java/com/findr/MainApplication.java
+++ app/android/app/src/main/java/com/findr/MainApplication.java
@@ -4,17 +4,12 @@ import android.app.Application;
 
 import com.facebook.react.ReactApplication;
 import com.horcrux.svg.SvgPackage;
-import com.imagepicker.ImagePickerPackage;
-import com.cmcewen.blurview.BlurViewPackage;
-import com.oblador.vectoricons.VectorIconsPackage;
-import com.BV.LinearGradient.LinearGradientPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
+import com.imagepicker.ImagePickerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-import com.reactnativecommunity.netinfo.NetInfoPackage;
-
 
 import java.util.Arrays;
 import java.util.List;
@@ -32,12 +27,8 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
             new SvgPackage(),
-            new ImagePickerPackage(),
-            new BlurViewPackage(),
-            new VectorIconsPackage(),
-            new LinearGradientPackage(),
             new RNGestureHandlerPackage(),
-            new NetInfoPackage()
+            new ImagePickerPackage()
       );
     }
 
