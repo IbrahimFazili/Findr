e2d54b2dbf9a195dd267d9ff901f013f5a399553
Aditi Dagar <aditi.dagar@mail.utoronto.ca>
2020-07-23 22:29:26 -0400

    fixed the delete user bug

--- backend/test/test.js
+++ backend/test/test.js
@@ -725,6 +725,7 @@ describe("Test Cases", () => {
                     
                 } catch (error) {
                     console.log(error);
+                    throw Error(error);
                 }
 
                 done(); 
